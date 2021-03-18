13c8a1c82e26141b6ca6224d662e33b1264c7d6f
Cristiano Muroni <muronislick@hotmail.com>
2021-03-17 22:41:55 -0300

    colocando a rota authenticate no try catch

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.json');
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.json");
 
 const router = express.Router();
 
 function generateToken(params = {}) {
-  return jwt.sign(params,authConfig.secret, {
+  return jwt.sign(params, authConfig.secret, {
     expiresIn: 3600,
-  } )
+  });
 }
 
 router.post("/registro", async (req, res) => {
@@ -21,35 +21,30 @@ router.post("/registro", async (req, res) => {
 
     user.password = undefined;
 
-    return res.send({ user,
-      token: generateToken({id: user.id}),      
-    });
+    return res.send({ user, token: generateToken({ id: user.id }) });
   } catch (err) {
     return res.status(400).send({ error: "Falha ao registrar usuário" });
   }
 });
 
-router.post('/authenticate', async (req, res) => {
-  const {email, password} = req.body;
-  const user = await (await User.findOne({email})).select('+passord');
+router.post("/authenticate", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await (await User.findOne({ email })).select("+passord");
 
-  if(!user){
-    return res.status(400).send({error:'Usuário não encontrado'});
-  } 
+    if (!user) {
+      return res.status(400).send({ error: "Usuário não encontrado" });
+    }
 
-  if(!await bcrypt.compare(password, user.password)){
-    return res.status(400).send({error:'Senha inválida'});
+    if (!(await bcrypt.compare(password, user.password))) {
+      return res.status(400).send({ error: "Senha inválida" });
+      user.password = undefined;
 
+      return res.json({ user, token: generateToken({ id: user.id }) });
+    }
+  } catch (error) {
+    console.log(error);
   }
-
-  user.password = undefined;
-
- 
-  return res.json({user,
-     token: generateToken({id: user.id}),
-    });
-})
-
-
+});
 
 module.exports = (app) => app.use("/auth", router);
